ee61cc8c6d358f29448c309efaa9bf324c06a3b3
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2023-11-13 16:16:04 +0700

    loop logic

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const assassin = [
+    { name: 'Leonard', id: '0' },
+    { name: 'Aziz', id: '1' },
+    { name: 'Daniswara', id: '2' },
+    { name: 'Andhika', id: '3' },
+    { name: 'Yahya', id: '4' },
+    { name: 'Rayhan', id: '5' },
+    { name: 'Wahyu', id: '6' },
+    { name: 'Wildan', id: '7' },
+  ];
+  const target = [
+    { name: 'Sarah', id: '0' },
+    { name: 'David', id: '1' },
+    { name: 'Amanda', id: '2' },
+    { name: 'Michael', id: '3' },
+    { name: 'Eva', id: '4' },
+    { name: 'Alex', id: '5' },
+    { name: 'Sophia', id: '6' },
+  ];
+  const targetArray = target.map((item) => item.name);
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={assassin}
+        numColumns={1}
+        keyExtractor={(item) => [item.id]}
+        renderItem={({item, index}) => (
+          <View style={styles.list}>
+            <Text>{item.name} will assassinate {targetArray[index] ? targetArray[index] : 'no one'}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -17,4 +45,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  list : {
+    display : 'flex',
+    flex : '2',
+    flexDirection : 'row', 
+  }
 });
